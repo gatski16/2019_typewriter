b634a48c43a1919676a4dce08a9dc1b84d94495f
Martin <martingatski@gmail.com>
2019-09-01 19:00:00 +0200

    works

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const text = document.querySelector(".typewritten").textContent;
+let i = 0;
+let type = "";
+
+document.getElementById("typewriter").innerHTML = "";
+
+function typeWriter() {
+    let nextCharacter = text.charAt(i);
+    type = type + nextCharacter;
+    document.getElementById("typewriter").innerHTML = type;
+    i++;
+    if (i < text.length) {
+        setTimeout(typeWriter, 250);
+
+
+    }
+}
+
+typeWriter();
